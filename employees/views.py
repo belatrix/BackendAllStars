537191de60af50f3da364dc4cf9d074b0e9d7761
Sergio Infante <rsinfante@gmail.com>
2016-04-08 13:40:51 -0500

    Add top view and endpoint

--- employees/views.py
+++ employees/views.py
@@ -3,6 +3,7 @@ from .serializers import EmployeeSerializer, EmployeeListSerializer
 from django.shortcuts import get_object_or_404, get_list_or_404
 from rest_framework import status
 from rest_framework.decorators import api_view
+from rest_framework.exceptions import APIException
 from rest_framework.response import Response
 
 
@@ -19,3 +20,14 @@ def employee(request, employee_id):
         employee = get_object_or_404(Employee, pk=employee_id)
         serializer = EmployeeSerializer(employee)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+@api_view(['GET', ])
+def top(request, kind, quantity):
+    try:
+        if request.method == 'GET':
+            employee_list = Employee.objects.order_by('-'+kind)[:quantity]
+            serializer = EmployeeListSerializer(employee_list, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+    except Exception as e:
+        raise APIException(e)
+
