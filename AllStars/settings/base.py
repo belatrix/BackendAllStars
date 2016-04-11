c7e9d6abeed5a88732f1c6cd1d595ab0dd1dcc57
Sergio Infante <raulsergio9@gmail.com>
2016-04-10 23:49:44 -0500

    Add create_auth_token in order to create authentication code with new users

--- AllStars/settings/base.py
+++ AllStars/settings/base.py
@@ -57,6 +57,9 @@ ROOT_URLCONF = 'AllStars.urls'
 
 # REST
 REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
--- employees/models.py
+++ employees/models.py
@@ -1,8 +1,12 @@
 from __future__ import unicode_literals
 
+from django.conf import settings
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.contrib.auth.models import AbstractUser
 from django.utils.encoding import python_2_unicode_compatible
+from rest_framework.authtoken.models import Token
 
 
 @python_2_unicode_compatible
@@ -23,3 +27,8 @@ class Employee(AbstractUser):
     avatar = models.ImageField(upload_to='avatar', null=True, blank=True)
     categories = models.ManyToManyField('categories.Category', blank=True)
 
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
