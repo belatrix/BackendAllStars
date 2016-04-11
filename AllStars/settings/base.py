8e8e8819c8f1639571d31b18052ff527f6fe7663
Sergio Infante <rsinfante@gmail.com>
2016-04-11 10:29:58 -0500

    Remove default permission IsAuthenticated

--- AllStars/settings/base.py
+++ AllStars/settings/base.py
@@ -75,9 +75,6 @@ CORS_EXPOSE_HEADERS = ('Access-Control-Allow-Origin',
 
 # REST
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
-    ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
