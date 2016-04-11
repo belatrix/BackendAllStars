d4d9f219074c45c91be3f61eb63bc230150eb616
Sergio Infante <rsinfante@gmail.com>
2016-04-11 09:08:09 -0500

    Add configurations for database, ssl, allowed_hosts and statics files to production settings

--- AllStars/settings/production.py
+++ AllStars/settings/production.py
@@ -8,3 +8,18 @@ from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
