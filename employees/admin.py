5f96869eab8aaddfd31634296adae1d05cfca4be
Sergio Infante <raulsergio9@gmail.com>
2016-04-14 02:42:36 -0500

    Add password change form to user admin page.

--- employees/admin.py
+++ employees/admin.py
@@ -6,7 +6,10 @@ from django.contrib.auth.forms import ReadOnlyPasswordHashField
 
 
 class UserChangeForm(forms.ModelForm):
-    password = ReadOnlyPasswordHashField()
+    password = ReadOnlyPasswordHashField(label= ("Password"),
+                                         help_text= ("Raw passwords are not stored, so there is no way to see "
+                                         "this user's password, but you can change the password using "
+                                         "<a href=\'../password/\'>this form</a>."))
 
     class Meta:
         model = Employee
