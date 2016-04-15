fe5fdd6b43c3dd4fe24f15b48d8e3a6bdf56a41c
Sergio Infante <raulsergio9@gmail.com>
2016-04-14 20:19:50 -0500

    Add search endpoint for employees

--- employees/urls.py
+++ employees/urls.py
@@ -1,5 +1,5 @@
 from .views import CustomObtainAuthToken, employee_list, employee
-from .views import top
+from .views import search, top
 from django.conf.urls import url
 
 
@@ -8,4 +8,5 @@ urlpatterns = [
     url(r'^list/$', employee_list, name='employee_list'),
     url(r'^list/top/(?P<kind>\w+)/(?P<quantity>\d+)/$', top, name='employee_list_top'),
     url(r'^(?P<employee_id>\d+)/$', employee, name='employee_detail'),
+    url(r'^search/(?P<search_term>\w+)/', search, name='employee_search'),
 ]
