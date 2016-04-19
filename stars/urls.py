26382dce7afa8964b2780ab2ece0e96306332730
Javier Valdivia <javivalag@gmail.com>
2016-04-19 21:58:31 +0000

    Adding endpoint to get a star list by employee

--- stars/urls.py
+++ stars/urls.py
@@ -1,8 +1,9 @@
-from .views import give_star_to, stars_employee_subcategory_list
+from .views import give_star_to, stars_employee_list, stars_employee_subcategory_list
 from django.conf.urls import url
 
 
 urlpatterns = [
     url(r'^(?P<from_employee_id>\d+)/give/star/to/(?P<to_employee_id>\d+)/', give_star_to, name='give_star_to'),
+    url(r'^(?P<employee_id>\d+)/list/$', stars_employee_list, name='stars_employee_list'),
     url(r'^(?P<employee_id>\d+)/subcategory/list/$', stars_employee_subcategory_list, name='stars_employee_subcategory_list'),
 ]
