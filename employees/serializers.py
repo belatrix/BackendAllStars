879ea9c4234a5d8435f213c6f9b082a86a794ecc
Sergio Infante <raulsergio9@gmail.com>
2016-04-22 12:34:24 -0500

    Add 1 level depth and also categories fields to employee serializer

--- employees/serializers.py
+++ employees/serializers.py
@@ -5,6 +5,7 @@ from rest_framework import serializers
 class EmployeeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Employee
+        depth = 1
         fields = ('pk',
                   'username',
                   'email',
@@ -16,6 +17,7 @@ class EmployeeSerializer(serializers.ModelSerializer):
                   'current_month_score',
                   'level',
                   'score',
+                  'categories',
                   'is_active',
                   'last_login')
 
