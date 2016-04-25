54ef1b63d990329fdbdae8211885fa8f6f8a47e4
Sergio Infante <raulsergio9@gmail.com>
2016-04-24 19:04:12 -0500

    Add /api/category/list/ endpoint

--- categories/urls.py
+++ categories/urls.py
@@ -1,6 +1,7 @@
-from .views import subcategories_list
+from .views import category_list, subcategories_list
 from django.conf.urls import url
 
 urlpatterns = [
+    url(r'^list/$', category_list, name='category_list'),
     url(r'^(?P<category_id>\d+)/subcategory/list/$', subcategories_list, name='subcategories_list')
 ]
--- categories/views.py
+++ categories/views.py
@@ -1,5 +1,6 @@
-from .models import Subcategory
-from .serializers import SubCategoryListSerializer
+from .models import Category, Subcategory
+from .serializers import CategorySerializer, SubCategoryListSerializer
+from django.shortcuts import get_list_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
@@ -10,4 +11,12 @@ def subcategories_list(request, category_id):
     if request.method == 'GET':
         subcategories = Subcategory.objects.filter(category=category_id)
         serializer = SubCategoryListSerializer(subcategories, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@api_view(['GET'])
+def category_list(request):
+    if request.method == 'GET':
+        categories = get_list_or_404(Category)
+        serializer = CategorySerializer(categories, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
