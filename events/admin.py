a2577f580742f6e54967ffb7ce5580f6ab47f30d
Sergio Infante <rsinfante@gmail.com>
2016-05-16 17:14:19 -0500

    Add details to admin events and improve models

--- events/admin.py
+++ events/admin.py
@@ -2,8 +2,17 @@ from .models import Event, Participant, Talk
 from django.contrib import admin
 
 
-admin.site.register(Event)
-admin.site.register(Participant)
-admin.site.register(Talk)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'datetime', 'location')
 
 
+class ParticipantAdmin(admin.ModelAdmin):
+    list_display = ('name', 'lastname', 'email')
+
+
+class TalkAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'datetime', 'speaker', 'location')
+
+admin.site.register(Event, EventAdmin)
+admin.site.register(Participant, ParticipantAdmin)
+admin.site.register(Talk, TalkAdmin)
--- events/models.py
+++ events/models.py
@@ -1,11 +1,14 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
 
+@python_2_unicode_compatible
 class Participant(models.Model):
     name = models.CharField(max_length=200)
     lastname = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200, unique=True)
     birth_date = models.DateField(blank=True, null=True)
     carreer = models.CharField(max_length=200, blank=True, null=True)
     educational_center = models.CharField(max_length=200, blank=True, null=True)
@@ -13,20 +16,35 @@ class Participant(models.Model):
     facebook_account = models.CharField(max_length=200, blank=True, null=True)
     twitter_account = models.CharField(max_length=200, blank=True, null=True)
 
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        verbose_name_plural = 'participants'
+        ordering = ['name', 'lastname', 'email']
+
 
 class Event(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=200, unique=True)
     description = models.CharField(max_length=200, blank=True, null=True)
     datetime = models.DateTimeField(blank=True, null=True)
     location = models.CharField(max_length=200, blank=True, null=True)
     collaborators = models.ManyToManyField('employees.Employee', blank=True)
     participants = models.ManyToManyField(Participant, blank=True)
 
+    class Meta:
+        verbose_name_plural = 'events'
+        ordering = ['-datetime', 'title']
+
 
 class Talk(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=200, unique=True)
     description = models.CharField(max_length=200, blank=True, null=True)
     datetime = models.DateTimeField(blank=True, null=True)
     location = models.CharField(max_length=200, blank=True, null=True)
-    speaker = models.ForeignKey('employees.Employee', blank=True)
+    speaker = models.ForeignKey('employees.Employee', blank=True, null=True)
     participants = models.ManyToManyField(Participant, blank=True)
+
+    class Meta:
+        verbose_name_plural = 'talks'
+        ordering = ['-datetime', 'title']
