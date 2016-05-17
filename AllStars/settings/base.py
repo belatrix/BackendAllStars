36d529a293e2d41aaa8d1dbba06c52c3ab587afe
Sergio Infante <rsinfante@gmail.com>
2016-05-17 16:50:21 -0500

    Allow just JSONRenderer as a default class

--- AllStars/settings/base.py
+++ AllStars/settings/base.py
@@ -85,6 +85,9 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ),
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 20
 }
