99264ea03acd9bc2f96fb8763781f0c5414842fd
Sergio Infante <rsinfante@gmail.com>
2016-05-30 14:01:51 -0500

    Add level to StarTopEmployeeList serializer

--- stars/serializers.py
+++ stars/serializers.py
@@ -63,6 +63,7 @@ class StarTopEmployeeLists(serializers.Serializer):
     username = serializers.CharField(max_length=100, source='to_user__username')
     first_name = serializers.CharField(max_length=100, source='to_user__first_name')
     last_name = serializers.CharField(max_length=100, source='to_user__last_name')
+    level = serializers.IntegerField(source='to_user__level')
     avatar = serializers.SerializerMethodField()
     num_stars = serializers.IntegerField()
 
