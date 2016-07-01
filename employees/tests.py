9da4bbe2c5d8dbfe6436ff4fe5e1387178009897
Sergio Infante <raulsergio9@gmail.com>
2016-07-01 13:16:22 -0500

    Fix category dependency for employee creation flow in testing

--- employees/tests.py
+++ employees/tests.py
@@ -1,9 +1,11 @@
 from .models import Employee
+from categories.models import Category
 from rest_framework.test import APITestCase
 
 
 class EmployeeTestCase(APITestCase):
     def setUp(self):
+        Category.objects.create(name='Coworker')
         Employee.objects.create_superuser('user1', 'user1@email.com', 'user1password')
         Employee.objects.create_superuser('user2', 'user2@email.com', 'user2password')
 
