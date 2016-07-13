204ee50b0caa98bc22e9c5be38edf4fbf51184b6
Sergio Infante <raulsergio9@gmail.com>
2016-07-13 14:25:10 -0500

    Add endpoint to send messages by location

--- activities/urls.py
+++ activities/urls.py
@@ -1,7 +1,8 @@
-from .views import send_message_all
+from .views import send_message_all, send_message_location
 from django.conf.urls import url
 
 
 urlpatterns = [
     url(r'^send/message/all/$', send_message_all, name='send_message_all'),
+    url(r'^send/message/location/(?P<location_id>\d+)/$', send_message_location, name='send_message_location'),
 ]
--- activities/views.py
+++ activities/views.py
@@ -1,8 +1,9 @@
 from .models import Message
 from .serializers import MessageSerializer
 from constance import config
-from employees.models import Employee, EmployeeDevice
+from employees.models import Employee, EmployeeDevice, Location
 from django.conf import settings
+from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
@@ -12,7 +13,7 @@ import requests
 
 def send_message_android(destination, title, message):
     headers = {
-        'Authorization': 'key='+settings.FIREBASE_SERVER_KEY,
+        'Authorization': 'key=' + settings.FIREBASE_SERVER_KEY,
         'Content - Type': 'application/json'
     }
     payload = {
@@ -67,6 +68,57 @@ def send_message_all(request):
                     if employee_devices.ios_device:
                         # TODO iOS push notification
                         pass
+                except:
+                    pass
+            serializer = MessageSerializer(message)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            content = {'detail': config.NO_MESSAGE}
+            return Response(content, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['POST', ])
+@permission_classes((IsAuthenticated,))
+def send_message_location(request, location_id):
+    """
+    Send message to all employees by location
+    ---
+    response_serializer: employees.serializers.EmployeeDeviceSerializer
+    parameters:
+    - name: message
+      type: string
+      required: true
+    responseMessages:
+    - code: 401
+      message: Unauthorized. Authentication credentials were not provided. Invalid token.
+    - code: 403
+      message: Forbidden.
+    - code: 404
+      message: Not found
+    - code: 500
+      message: Internal Server Error
+    """
+    if request.method == 'POST':
+        if 'message' in request.data:
+            employee_list = Employee.objects.all()
+            location = get_object_or_404(Location, pk=location_id)
+            message = Message(
+                text=request.data['message'],
+                from_user=request.user,
+                to_user="location: " + location.name)
+            message.save()
+            for employee in employee_list:
+                try:
+                    employee_devices = EmployeeDevice.objects.get(username=employee)
+                    if employee.location == location:
+                        if employee_devices.android_device:
+                            send_message_android(
+                                employee_devices.android_device,
+                                config.TITLE_PUSH_NOTIFICATION,
+                                message.text)
+                        if employee_devices.ios_device:
+                            # TODO iOS push notification
+                            pass
                 except:
                     pass
             serializer = MessageSerializer(message)
