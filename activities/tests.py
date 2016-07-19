570072083104ae53d5c89b9d7f1286faba787e33
Sergio Infante <raulsergio9@gmail.com>
2016-07-19 08:30:33 -0500

    Add sample testcases for models

--- activities/tests.py
+++ activities/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from .models import Activity
+from categories.models import Category
+from employees.models import Employee
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class ActivityTestCase(APITestCase):
+    def setUp(self):
+        Category.objects.create(name='Coworker')
+        Employee.objects.create_superuser('user1', 'user1@email.com', 'user1password')
+        Employee.objects.create_superuser('user2', 'user2@email.com', 'user2password')
+
+    def test_activity_creation(self):
+        activity = Activity(text='foobar activity')
+        self.assertEqual(activity.text, 'foobar activity')
--- stars/tests.py
+++ stars/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+from .models import Star
+from categories.models import Category, Subcategory, Keyword
+from employees.models import Employee
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class ActivityTestCase(APITestCase):
+    def setUp(self):
+        Category.objects.create(name='Coworker')
+        Subcategory.objects.create(name='Demo')
+        Keyword.objects.create(name='foobar')
+        Employee.objects.create_superuser('user1', 'user1@email.com', 'user1password')
+        Employee.objects.create_superuser('user2', 'user2@email.com', 'user2password')
+
+    def test_star_creation(self):
+        employee1 = Employee.objects.get(username='user1')
+        employee2 = Employee.objects.get(username='user2')
+        category1 = Category.objects.get(name='Coworker')
+        subcategory1 = Subcategory.objects.get(name='Demo')
+        keyword1 = Keyword.objects.get(name='foobar')
+        star = Star(
+            text='foobar',
+            from_user=employee1,
+            to_user=employee2,
+            category=category1,
+            subcategory=subcategory1,
+            keyword=keyword1)
+        self.assertEqual(star.text, 'foobar')
