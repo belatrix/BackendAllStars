698797aafb68ab2a1e308b35774418ca8d2e7443
Sergio Infante <raulsergio9@gmail.com>
2016-12-21 13:29:03 -0500

    Refactor administrator endpoint in order to remove subcategory model

--- administrator/serializers.py
+++ administrator/serializers.py
@@ -2,57 +2,11 @@ from categories.models import Category, Keyword
 from rest_framework import serializers
 
 
-class CategoryListSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Category
-        fields = ('pk', 'name')
-
-
-class SubcategoryListSerializer(serializers.ModelSerializer):
-    class Meta:
-        # model = Subcategory
-        fields = ('pk', 'name')
-
-
-class CategorySimpleSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Category
-
-
 class CategorySerializer(serializers.ModelSerializer):
-    subcategories = SubcategoryListSerializer(many=True, source='subcategory_set')
-
     class Meta:
         model = Category
-        fields = ('pk', 'name', 'weight', 'is_active', 'comment_required', 'subcategories')
 
 
 class KeywordSerializer(serializers.ModelSerializer):
     class Meta:
         model = Keyword
-
-
-class SubcategorySimpleSerializer(serializers.ModelSerializer):
-    categories = CategoryListSerializer(many=True, source='category')
-
-    class Meta:
-        # model = Subcategory
-        fields = ('pk', 'name', 'is_active', 'categories')
-
-
-class SubcategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        # model = Subcategory
-        fields = ('pk', 'name', 'is_active')
-
-
-class CategoryPKListSerializer(serializers.Serializer):
-    categories = serializers.ListField(
-        child=serializers.IntegerField()
-    )
-
-
-class SubcategoryPKListSerializer(serializers.Serializer):
-    subcategories = serializers.ListField(
-        child=serializers.IntegerField()
-    )
--- administrator/urls.py
+++ administrator/urls.py
@@ -1,13 +1,14 @@
 from django.conf.urls import url
-from .views import CategoryDetail, CategoryList, KeywordDetail, KeywordList, SubcategoryDetail, SubcategoryList
-from .views import DeleteCategories
+from .views import CategoryDetail, CategoryList, KeywordDetail, KeywordList
+from .views import CategoriesModelsDelete
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    url(r'^delete/(?P<kind>\w+)/(?P<id>[0-9]+)/$', DeleteCategories.as_view()),
+    url(r'^delete/categories/(?P<kind>\w+)/(?P<id>[0-9]+)/$', CategoriesModelsDelete.as_view()),
     url(r'^category/$', CategoryList.as_view()),
     url(r'^category/(?P<category_id>[0-9]+)/$', CategoryDetail.as_view()),
     url(r'^keyword/$', KeywordList.as_view()),
     url(r'^keyword/(?P<keyword_id>[0-9]+)/$', KeywordDetail.as_view()),
-    url(r'^subcategory/$', SubcategoryList.as_view()),
-    url(r'^subcategory/(?P<subcategory_id>[0-9]+)/$', SubcategoryDetail.as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns, allowed=['json'])
