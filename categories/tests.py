1a83a1dbcbefce7fbecf52231d9759c357c97ecd
Sergio Infante <raulsergio9@gmail.com>
2017-01-05 12:34:04 -0500

    Remove category.weight attribute from categories tests

--- categories/tests.py
+++ categories/tests.py
@@ -7,12 +7,12 @@ class CategoryTestCase(APITestCase):
     def setUp(self):
         Category.objects.create(name='Coworker')
         Employee.objects.create_superuser('user1', 'user1@email.com', 'user1password')
-        Category.objects.create(name='Category1', weight=2)
+        Category.objects.create(name='Category1')
         Category.objects.create(name='Category2')
         self.client.login(username='user1', password='user1password')
 
     def test_category_creation(self):
         category1 = Category.objects.get(name='Category1')
         category2 = Category.objects.get(name='Category2')
-        self.assertEqual(category1.weight, 2)
-        self.assertEqual(category2.weight, 1)
+        self.assertEqual(category1.name, 'Category1')
+        self.assertEqual(category2.name, 'Category2')
