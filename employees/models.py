a5296fa9182f50dd903f7904234922f96726b53a
Sergio Infante <raulsergio9@gmail.com>
2017-01-05 08:21:50 -0500

    Add is_active fields to position and role models

--- employees/models.py
+++ employees/models.py
@@ -27,6 +27,7 @@ class Location(models.Model):
 @python_2_unicode_compatible
 class Role(models.Model):
     name = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name
@@ -41,6 +42,7 @@ class Position(models.Model):
     name = models.CharField(max_length=100)
     weight = models.PositiveSmallIntegerField(default=1)
     comment_required = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name
