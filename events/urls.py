4cee23ba456cd33515c2108cd0b6a131fce2eb66
Sergio Infante <raulsergio9@gmail.com>
2017-01-09 13:19:07 -0500

    Add other location events endpoint

--- events/urls.py
+++ events/urls.py
@@ -1,8 +1,9 @@
-from .views import my_upcoming_events, local_events
+from .views import my_upcoming_events, local_events, other_location_events
 from django.conf.urls import url
 
 
 urlpatterns = [
     url(r'^employee/(?P<employee_id>\d+)/upcoming/', my_upcoming_events, name='my_upcoming_events'),
     url(r'^employee/(?P<employee_id>\d+)/local/', local_events, name='local_events'),
+    url(r'^employee/(?P<employee_id>\d+)/others/', other_location_events, name='other_location_events'),
 ]
--- events/views.py
+++ events/views.py
@@ -16,11 +16,11 @@ def my_upcoming_events(request, employee_id):
     serializer: events.serializers.EventSerializer
     responseMessages:
     - code: 401
-     message: Unauthorized. Authentication credentials were not provided. Invalid token.
+      message: Unauthorized. Authentication credentials were not provided. Invalid token.
     - code: 403
-     message: Forbidden.
+      message: Forbidden.
     - code: 404
-     message: Not found
+      message: Not found
     """
     events = []
     if request.method == 'GET':
@@ -47,16 +47,47 @@ def local_events(request, employee_id):
     serializer: events.serializers.EventSerializer
     responseMessages:
     - code: 401
-     message: Unauthorized. Authentication credentials were not provided. Invalid token.
+      message: Unauthorized. Authentication credentials were not provided. Invalid token.
     - code: 403
-     message: Forbidden.
+      message: Forbidden.
     - code: 404
-     message: Not found
+      message: Not found
     """
-    events = []
     if request.method == 'GET':
         employee = get_object_or_404(Employee, pk=employee_id)
         events = Event.objects.filter(location=employee.location, is_active=True, is_upcoming=True)
+        paginator = PageNumberPagination()
+        results = paginator.paginate_queryset(events, request)
+        serializer = EventSerializer(results, many=True)
+        return paginator.get_paginated_response(serializer.data)
+
+
+@api_view(['GET', ])
+@permission_classes((IsAuthenticated,))
+def other_location_events(request, employee_id):
+
+    """
+    Returns the full upcoming events list for employee location
+    ---
+    serializer: events.serializers.EventSerializer
+    responseMessages:
+    - code: 401
+      message: Unauthorized. Authentication credentials were not provided. Invalid token.
+    - code: 403
+      message: Forbidden.
+    - code: 404
+      message: Not found
+    """
+
+    events = []
+    if request.method == 'GET':
+        employee = get_object_or_404(Employee, pk=employee_id)
+        events_list = Event.objects.filter(is_active=True, is_upcoming=True)
+
+        for event in events_list:
+            if event.location != employee.location:
+                events.append(event)
+
         paginator = PageNumberPagination()
         results = paginator.paginate_queryset(events, request)
         serializer = EventSerializer(results, many=True)
