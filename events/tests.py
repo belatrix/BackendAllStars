d789287198fd90682d5f0d07f343149c0d672f54
Sergio Infante <raulsergio9@gmail.com>
2017-01-16 14:04:33 -0500

    Add creation event test

--- events/tests.py
+++ events/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from .models import Event
+from employees.models import Location
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class EventTestCase(APITestCase):
+    def setUp(self):
+        location = Location.objects.create(name='location 1')
+        Event.objects.create(name='Event 1', location=location)
+        Event.objects.create(name='Event 2', location=location)
+
+    def test_event_creation(self):
+        location = Location.objects.get(name='location 1')
+        event1 = Event.objects.get(name='Event 1')
+        self.assertEqual(event1.name, 'Event 1')
+        self.assertEqual(event1.location, location)
